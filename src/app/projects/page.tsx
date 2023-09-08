072b3aec5bfc6241f5990ae332212b1db85ba80e
Daniel Ramírez <daniel_ramirezs@hotmail.com>
2023-09-08 00:16:28 -0600

    fixed height on home screen and made responsive projects layout

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,11 +1,16 @@
 'use client'
+import { useGlobalLanguage } from '../context/LanguageContext'
 
 import './projects.css'
 
 export default function Projects() {
+
+  const {language} = useGlobalLanguage()
   return (
     <>
-    <div className='title-big title-white title-decorated title-centered bg-deep-blue'> Mis proyectos. </div>
+    <div className='title-big title-white title-decorated title-centered bg-deep-blue'>
+      {language === 'EN' ? titleEn : titleEs}
+    </div>
     <section className="projects-container">
       <div className='card'>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim veniam sequi nisi officia animi dolor optio perspiciatis similique ad quia inventore ea, vitae odio nostrum libero tempora voluptatum dolorem id!
@@ -27,3 +32,6 @@ export default function Projects() {
     </>
   )
 }
+
+const titleEs : string = 'Mis Proyectos.'
+const titleEn : string = 'My Projects.'
